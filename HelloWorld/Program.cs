821a0d8afd0e6f976ba756ee89411b0d269d5564
Felipe Kureski <81197531+Fkureski@users.noreply.github.com>
2025-03-19 17:40:25 -0300

    Primitive Types and Expressions

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //byte number = 2;
+            //int count = 10;
+            //float totalPrice = 20.95f;
+            //char character = 'a';
+            //string firstName = "Felipe";
+            //bool isWorking = false;
+
+            ////var number = 2;
+            ////var count = 10;
+            ////var totalPrice = 20.95f;
+            ////var character = 'a';
+            ////var firstName = "Felipe";
+            ////var isWorking = false;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(count);
+            //Console.WriteLine(totalPrice);
+            //Console.WriteLine(firstName);
+            //Console.WriteLine(isWorking);
+            //Console.WriteLine(character);
+
+
+            //Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            //Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+
+            //const float Pi = 3.14f;
+
+
+            //byte b = 1;
+            //int i = b;
+
+            //int i = 1;
+            //byte b = i;
+
+            //int i = 1000;
+            //byte b = (byte) i;
+
+            //Console.WriteLine(b);
+
+            //string number = "1234";
+            //int i = Convert.ToInt32(number);
+            //byte b = Convert.ToByte(number);
+
+            //Console.WriteLine(i);
+
+            //    try
+            //    {
+            //        string number = "1234";
+            //        int i = Convert.ToInt32(number);
+            //        byte b = Convert.ToByte(number);
+
+            //        Console.WriteLine(b);
+            //    }
+            //    catch (Exception ex) {
+            //        Console.WriteLine("The number could not be converted to a byte.");
+            //}
+
+            //try{
+            //    string str = "true";
+            //    bool b = Convert.ToBoolean(str);
+            //    Console.WriteLine(b);
+
+            //} catch{
+            //    Console.WriteLine("The number could not be converted to a boolean.");
+            //}
+
+            //var a = 10;
+            //var b = 3;
+
+            //Console.WriteLine((float)a / (float)b);
+
+            //var a = 1;
+            //var b = 2;
+            //var c = 3;
+
+            //Console.WriteLine(a + b * c);
+            //Console.WriteLine((a + b) * c);
+            //Console.WriteLine(a > b);
+            //Console.WriteLine(a == b);
+            //Console.WriteLine(a != b);
+            //Console.WriteLine(!(a != b));
+            //Console.WriteLine(c > b && c > a);
+        }
+    }
+}
